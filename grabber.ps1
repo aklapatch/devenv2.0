230aa679838a8a409fe54b14d7c513cbb619c023
U-DESKTOP-F54A5DR\me <sniper96a@gmail.com>
2019-02-07 16:51:52 -0500

    fixed checkArgs and listRecipies

--- grabber.ps1
+++ grabber.ps1
@@ -1,19 +1,30 @@
 # two options, install and remove
 
-$operations= "add", "drop"
-$recipiedir= "recipies\"
+$global:operations=@("add","drop")
+$global:recipiedir= -join($PSScriptRoot,"\recipies")
 
-# lists available recipies and prints them
-function checkRecipies {
+# gets available recipies and prints them
+function listRecipies {
 	
+	if (-Not (Test-Path $global:recipiedir)) {
+		Write-Output "recipiedir is not valid"
+	}
+	
+	echo "Possible recipies are: "
+
+	$rep_len=$global:recipiedir.Length
+	Get-ChildItem $global:recipiedir -Filter *.ps1 | 	Foreach-Object {
+		$diff_len=$_.FullName.Length - $rep_len -1
+		echo $_.FullName.substring($rep_len+1, $diff_len-4)	
+	} 
 }
 
 # --------------------------------------------------------------#
-function checkArgs {
-	$var=$args[1]
+function checkArgs($operation,$package) {
 	
 	# check for the second arg or package
-	if ( -Not ($args[0] -in $global:operations) ) {
+	if ( [string]::IsNullOrEmpty($operation) ) {
+		echo
 		Write-Output "Usage: $PSCommandPath operation packageName"
 		Write-Output "Possible operations: add (retrieves and installs packages)"
 		Write-Output "                     drop (uninstalls package)"
@@ -21,14 +32,29 @@ function checkArgs {
 	}
 	
 	# check for the second arg or package
-	if ( [string]::IsNullOrEmpty($args[1]) ) {
+	if (  -Not ($operation -in $global:operations) ) {
+		Write-Output "Usage:    $PSCommandPath operation packageName"
+		Write-Output "Possible operations: add (retrieves and installs packages)"
+		Write-Output "                     drop (uninstalls package)"
+		Exit
+	}
+	
+	# check for the second arg or package
+	if ( [string]::IsNullOrEmpty($package) ) {
 		Write-Output "Please specify what package you want to $args"
+		
+		# print possibilities
+		listRecipies
 		Exit
 	}
 	
 	# check if a package is available
-	if ( -Not ( Test-Path (-join($global:recipiedir,$var)) -PathType Leaf) ) {
-		Write-Output "Recipie for $var not found."
+	$reppath= "$global:recipiedir\$($package)$(".ps1")"
+	if ( -Not ( Test-Path $reppath) ) {
+		Write-Output "Recipie for $package not found in $global:recipiedir"
+
+		# print possibilities
+		listRecipies
 		Exit
 	}
 }
@@ -38,6 +64,8 @@ function checkArgs {
 # check args and usage
 checkArgs $args[0] $args[1]
 
+listRecipies 
+
 # go to script's location
 cd $PSScriptRoot
 
@@ -45,12 +73,14 @@ cd $PSScriptRoot
 $operation=$args[0]
 $package=$args[1]
 
-Write-Output $args[0]
+# in the 'add' case, check the installed folder to see if the package is installed. 
+#If it is installed, then let the user know and exit
+# if it is not installed, the recipie will download and extract the files
+# to the tmp dir. Then this file will make a list of the files for
+# that package with 'getFiles.bat', and check for conflicts.
+# that list is put in recipies\installed. Then the files are moved
+# from the tmp dir to the 'root' directory
 
-if ($operation -eq "add"){
-	
+# in the 'drop' case, check if the package is installed. If so, 
+# then delete all the files on the file list. If not, then let the user know and exit.
 
-	
-	
-
-}
