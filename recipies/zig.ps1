7ef55dfedbb4507356de584f1589046cd0f73db8
U-DESKTOP-F54A5DR\me <sniper96a@gmail.com>
2019-05-13 17:06:18 -0400

    changed stuff

--- recipies/zig.ps1
+++ recipies/zig.ps1
@@ -1,5 +1,5 @@
 # package details
-$pkgname="cmake"
+$pkgname="zig"
 
 # required dependencies
 $requires=@("7zip")
@@ -10,40 +10,44 @@ $download_name="$pkgname.zip"
 function arrange($fname, $exdir) {
 
 	7z x "$($fname)" -o"$exdir"  | out-null
-	
-	Get-ChildItem -Path "$exdir\cmake-*win64*\*" -Force | Move-Item -Destination "$exdir"	
+
+	Get-ChildItem -Path "$exdir\zig-windows*\*" -Force | Move-Item -Destination "$exdir"
 
 	# remove directory
-	Remove-Item "$exdir\cmake-*win64*"
+	Remove-Item "$exdir\zig-windows-*" -Force
 }
 
 function getInfo() {
 	# go to the download page and download the first file from top to bottom
 	# that has the right pattern
 	
-	$base_url="https://cmake.org/download/"
+	$base_url="https://ziglang.org/download/"
 
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-	$site=Invoke-WebRequest -uri $base_url
+	$site=iwr -uri "$base_url"
+	$Lnum = 0
 	$links=$site.parsedhtml.getelementsbytagname("a")
 	foreach ($link in $links) {
 		if($link.tagName -eq "A") {
-			if($link.href -Like "*cmake-*-win64-x64.zip"){
-			
+			if($link.href -Like "*zig-windows-x86_64-*.zip"){
+				
 				$download_url=$link.href.Trim()
-				break
+				
+				# only get the second link
+				if ($Lnum -gt 0){
+					break
+				}
+				$Lnum += 1
 			}
 		}
 	}
 	
-	# get the version (get text in between jdk- and the _ 
-	$dex1=$download_url.indexof('-')
-	$dex2=$download_url.indexof('-win')
+	# get the version (get text in between 64- and the .zip
+	$dex1=$download_url.indexof('4-')
+	$dex2=$download_url.indexof('.zip')
 	
 	$len=$dex2-$dex1
 	
-	$version=$download_url.substring($dex1+1,$len-1)
+	$version=$download_url.substring($dex1+2,$len-2)
 	
 	return @($version,$download_url)
 }
